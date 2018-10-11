2170b1f644d141b9f6bd7b1e783a56bfd8ceb594
SNiemiec <slawekniemiec@gmail.com>
2018-10-11 14:55:39 +0200

    In the middle of major changes: including todo list

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var addArray, submitArray;
+var addArray, submitArray, openArray ;
 
 addArray = Array.prototype.slice.call(document.querySelectorAll(".add"));
 
@@ -6,48 +6,112 @@ submitArray = Array.prototype.slice.call(
   document.querySelectorAll(".submit-btn")
 );
 
-//OBJECT PROTOTYPE CONSTRUCTOR
+openArray = Array.prototype.slice.call(
+  document.querySelectorAll(".open")
+);
 
-function DayList(a, b, c, d, e, f) {
-  this.a = a;
-  this.b = b;
-  this.c = c;
-  this.d = d;
-  this.e = e;
-  this.f = f;
+
+class DayList {
+  constructor(a, b, f, g) {
+    this.a = a;
+    this.b = b;
+    this.f = f;
+    this.g = g;
+  }
 }
+
+//TODO SECTION
+
+
+
+var button_tasks = document.getElementById('enter');
+var input_add = document.getElementById('input_list');
+var ul_tasks = document.getElementById('task_list');
+var mynotes = document.getElementById('mynotes');
+
+
+function inputLength() {
+  return input_add.value.length
+}
+
+function createListElement() {
+
+  var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input_add.value));
+    ul_tasks.appendChild(li);
+    input_add.value = '';
+
+}
+
+function addListAfterClick() {
+
+  if (inputLength() > 0) {
+    createListElement();
+  }
+
+}
+
+function addListAfterEnter(event) {
+
+  if (inputLength() > 0 && event.keyCode === 13) {
+
+    createListElement();
+  }
+}
+
+button_tasks.addEventListener("click", addListAfterClick );
+
+input_add.addEventListener("keypress", addListAfterEnter);
+
+
 
 //VIEW METHODS
 
 var view = {
-  objectBuilder: function() {
+
+  objectBuilder: function () {
     notes1 = document.getElementById("input2").value;
     morenotes1 = document.getElementById("input3").value;
-    task1 = document.getElementById("input4").value;
-    task2 = document.getElementById("input5").value;
-    task3 = document.getElementById("input6").value;
     radios = document.getElementsByName("day");
     for (var i = 0, length = radios.length; i < length; i++) {
       if (radios[i].checked) {
         checkedRadio = radios[i].value;
         break;
       }
-    }
+    };
+    tasks4 = ul_tasks.innerHTML;
   },
 
-  disableAdd: function() {
+  todoListRemove: function() {
+    var todolist = document.getElementById('todolist');
+    todolist.remove();
+  },
+  
+  disableAdd: function () {
     for (var i = 0; i < addArray.length; i++) {
       addArray[i].disabled = true;
     }
   },
 
-  enableAdd: function() {
+  disableOpen: function () {
+    for (var i = 0; i < openArray.length; i++) {
+      openArray[i].disabled = true;
+    }
+  },
+
+  enableAdd: function () {
     for (var i = 0; i < addArray.length; i++) {
       addArray[i].disabled = false;
     }
   },
 
-  undisplayForm: function() {
+  enableOpen: function () {
+    for (var i = 0; i < openArray.length; i++) {
+      openArray[i].disabled = false;
+    }
+  },
+
+  undisplayForm: function () {
     for (var i = 0; i < submitArray.length; i++) {
       submitArray[i].style.display = "none";
 
@@ -58,7 +122,7 @@ var view = {
 
 document.addEventListener(
   "click",
-  function(event) {
+  function (event) {
     if (event.target.id.includes("btn_")) {
       view.enableAdd();
       view.undisplayForm();
@@ -71,10 +135,8 @@ document.addEventListener(
           day1List = new DayList(
             notes1,
             morenotes1,
-            task1,
-            task2,
-            task3,
-            checkedRadio
+            checkedRadio,
+            tasks4
           );
           document.getElementById("btns_1_sub").style.display = "block";
           document.querySelector("#output1").innerHTML = day1List.a;
@@ -84,10 +146,8 @@ document.addEventListener(
           day2List = new DayList(
             notes1,
             morenotes1,
-            task1,
-            task2,
-            task3,
-            checkedRadio
+            checkedRadio,
+            tasks4
           );
           document.getElementById("btns_2_sub").style.display = "block";
           document.querySelector("#output1_2").innerHTML = day2List.a;
@@ -97,10 +157,8 @@ document.addEventListener(
           day3List = new DayList(
             notes1,
             morenotes1,
-            task1,
-            task2,
-            task3,
-            checkedRadio
+            checkedRadio,
+            tasks4
           );
           document.getElementById("btns_3_sub").style.display = "block";
           document.querySelector("#output1_3").innerHTML = day3List.a;
@@ -110,10 +168,8 @@ document.addEventListener(
           day4List = new DayList(
             notes1,
             morenotes1,
-            task1,
-            task2,
-            task3,
-            checkedRadio
+            checkedRadio,
+            tasks4
           );
           document.getElementById("btns_4_sub").style.display = "block";
           document.querySelector("#output1_4").innerHTML = day4List.a;
@@ -123,10 +179,8 @@ document.addEventListener(
           day5List = new DayList(
             notes1,
             morenotes1,
-            task1,
-            task2,
-            task3,
-            checkedRadio
+            checkedRadio,
+            tasks4
           );
           document.getElementById("btns_5_sub").style.display = "block";
           document.querySelector("#output1_5").innerHTML = day5List.a;
@@ -136,10 +190,8 @@ document.addEventListener(
           day6List = new DayList(
             notes1,
             morenotes1,
-            task1,
-            task2,
-            task3,
-            checkedRadio
+            checkedRadio,
+            tasks4
           );
           document.getElementById("btns_6_sub").style.display = "block";
           document.querySelector("#output1_6").innerHTML = day6List.a;
@@ -149,10 +201,8 @@ document.addEventListener(
           day7List = new DayList(
             notes1,
             morenotes1,
-            task1,
-            task2,
-            task3,
-            checkedRadio
+            checkedRadio,
+            tasks4
           );
           document.getElementById("btns_7_sub").style.display = "block";
           document.querySelector("#output1_7").innerHTML = day7List.a;
@@ -165,12 +215,10 @@ document.addEventListener(
     } else if (event.target.id.includes("-button")) {
       document.getElementById("input2").value = "";
       document.getElementById("input3").value = "";
-      document.getElementById("input4").value = "";
-      document.getElementById("input5").value = "";
-      document.getElementById("input6").value = "";
       document.getElementById("work").checked = true;
       document.getElementById("form-container").style.display = "flex";
       document.querySelector(".notes-box").style.display = "none";
+      ul_tasks.innerHTML = "";
       view.disableAdd();
 
       var eventbtn_add = event.target.id;
@@ -202,45 +250,40 @@ document.addEventListener(
       }
     } else if (event.target.id.includes("_sub")) {
       document.querySelector(".notes-box").style.display = "flex";
-
       view.undisplayForm();
+      view.disableOpen();
       var eventbtn_open = event.target.id;
+      var word = day1List.g;
+      var todoHtml = `<div id='todolist' class='notes-item'>Your tasks:<ul>${word}</ul></div>`;
 
       if (eventbtn_open === "btns_1_sub") {
+        
         document.querySelector("#output2").innerHTML = day1List.b;
-        document.querySelector("#output3").innerHTML = day1List.c;
-        document.querySelector("#output4").innerHTML = day1List.d;
-        document.querySelector("#output5").innerHTML = day1List.e;
+        mynotes.insertAdjacentHTML('afterend', todoHtml);
       } else if (eventbtn_open === "btns_2_sub") {
         document.querySelector("#output2").innerHTML = day2List.b;
-        document.querySelector("#output3").innerHTML = day2List.c;
-        document.querySelector("#output4").innerHTML = day2List.d;
-        document.querySelector("#output5").innerHTML = day2List.e;
+        mynotes.insertAdjacentHTML('afterend', "<div id='todolist'  class='notes-item'>Your tasks:<ul>" + day2List.g + "</ul></div>");
+        
       } else if (eventbtn_open === "btns_3_sub") {
         document.querySelector("#output2").innerHTML = day3List.b;
-        document.querySelector("#output3").innerHTML = day3List.c;
-        document.querySelector("#output4").innerHTML = day3List.d;
-        document.querySelector("#output5").innerHTML = day3List.e;
+        mynotes.insertAdjacentHTML('afterend', "<div id='todolist' class='notes-item'>Your tasks:<ul>" + day3List.g + "</ul></div>");
+        
       } else if (eventbtn_open === "btns_4_sub") {
         document.querySelector("#output2").innerHTML = day4List.b;
-        document.querySelector("#output3").innerHTML = day4List.c;
-        document.querySelector("#output4").innerHTML = day4List.d;
-        document.querySelector("#output5").innerHTML = day4List.e;
+        mynotes.insertAdjacentHTML('afterend', "<div id='todolist' class='notes-item'>Your tasks:<ul>" + day4List.g + "</ul></div>");
+        
       } else if (eventbtn_open === "btns_5_sub") {
         document.querySelector("#output2").innerHTML = day5List.b;
-        document.querySelector("#output3").innerHTML = day5List.c;
-        document.querySelector("#output4").innerHTML = day5List.d;
-        document.querySelector("#output5").innerHTML = day5List.e;
+        mynotes.insertAdjacentHTML('afterend', "><div id='todolist' class='notes-item'>Your tasks:<ul>" + day5List.g + "</ul></div>");
+        
       } else if (eventbtn_open === "btns_6_sub") {
         document.querySelector("#output2").innerHTML = day6List.b;
-        document.querySelector("#output3").innerHTML = day6List.c;
-        document.querySelector("#output4").innerHTML = day6List.d;
-        document.querySelector("#output5").innerHTML = day6List.e;
+        mynotes.insertAdjacentHTML('afterend', "<div id='todolist' class='notes-item'Your tasks:><ul>" + day6List.g + "</ul></div>");
+        
       } else if (eventbtn_open === "btns_7_sub") {
         document.querySelector("#output2").innerHTML = day7List.b;
-        document.querySelector("#output3").innerHTML = day7List.c;
-        document.querySelector("#output4").innerHTML = day7List.d;
-        document.querySelector("#output5").innerHTML = day7List.e;
+        mynotes.insertAdjacentHTML('afterend', "><div id='todolist' class='notes-item'>Your tasks:<ul>" + day7List.g + "</ul></div>");
+        
       } else {
         return;
       }
@@ -251,107 +294,71 @@ document.addEventListener(
 
 //CLOSE DAY NOTES
 
-document.getElementById("close").onclick = function() {
-document.querySelector(".notes-box").style.display = "none";
-view.enableAdd();
+
+document.getElementById("close").onclick = function () {
+  document.querySelector(".notes-box").style.display = "none";
+  view.todoListRemove();
+  view.enableAdd();
+  view.enableOpen();
 };
 
 //CLOSE FORM
 
-document.getElementById("close-form").onclick = function() {
-view.enableAdd();
-view.undisplayForm();
+document.getElementById("close-form").onclick = function () {
+  view.enableAdd();
+  view.undisplayForm();
 };
 
 
-window.onload = function() {
+window.onload = function () {
 
   var dayWeek = new Date();
   var currentDay = dayWeek.getDay();
-  
-  if(currentDay === 1) {
+
+  if (currentDay === 1) {
 
     document.getElementById('name1').innerHTML = 'Today';
     document.getElementById('name1').style.backgroundColor = '#FFE4B2';
 
+  } else if (currentDay === 2) {
+
+    document.getElementById('box1').style.order = 8;
+    document.getElementById('name2').innerHTML = 'today';
+    document.getElementById('name2').style.backgroundColor = '#FFE4B2';
+
+  } else if (currentDay === 3) {
+
+    document.getElementById('box1').style.order = 8;
+    document.getElementById('box2').style.order = 9;
+    document.getElementById('name3').innerHTML = 'today';
+    document.getElementById('name3').style.backgroundColor = '#FFE4B2';
+
+  } else if (currentDay === 4) {
+
+    document.getElementById('box1').style.order = 8;
+    document.getElementById('box2').style.order = 9;
+    document.getElementById('box3').style.order = 10;
+    document.getElementById('name4').innerHTML = 'Today';
+    document.getElementById('name4').style.backgroundColor = '#FFE4B2';
+  } else if (currentDay === 5) {
+
+    document.getElementById('box5').style.order = -3;
+    document.getElementById('box6').style.order = -2;
+    document.getElementById('box7').style.order = -1;
+    document.getElementById('name5').innerHTML = 'Today';
+    document.getElementById('name5').style.backgroundColor = '#FFE4B2';
+
+  } else if (currentDay === 6) {
+
+    document.getElementById('box6').style.order = -2;
+    document.getElementById('box7').style.order = -1;
+    document.getElementById('name6').innerHTML = 'Today';
+    document.getElementById('name6').style.backgroundColor = '#FFE4B2';
+  } else if (currentDay === 7) {
+
+    document.getElementById('box7').style.order = -1;
+    document.getElementById('name7').innerHTML = 'Today';
+    document.getElementById('name7').style.backgroundColor = '#FFE4B2';
   }
-
-else if(currentDay === 2) {
-
-document.getElementById('box1').style.order = 8;
-document.getElementById('name2').innerHTML = 'today';
-document.getElementById('name2').style.backgroundColor = '#FFE4B2';
-
-} else if(currentDay === 3) {
-
-  document.getElementById('box1').style.order = 8;
-  document.getElementById('box2').style.order = 9;
-  document.getElementById('name3').innerHTML = 'today';
-  document.getElementById('name3').style.backgroundColor = '#FFE4B2';
-  
-} else if(currentDay === 4) {
-
-  document.getElementById('box1').style.order = 8;
-  document.getElementById('box2').style.order = 9;
-  document.getElementById('box3').style.order = 10;
-  document.getElementById('name4').innerHTML = 'Today';
-  document.getElementById('name4').style.backgroundColor = '#FFE4B2';
-} else if(currentDay === 5) {
-
-  document.getElementById('box5').style.order = -3;
-  document.getElementById('box6').style.order = -2;
-  document.getElementById('box7').style.order = -1;
-  document.getElementById('name5').innerHTML = 'Today';
-  document.getElementById('name5').style.backgroundColor = '#FFE4B2';
-
-} else if(currentDay === 6) {
-
-  document.getElementById('box6').style.order = -2;
-  document.getElementById('box7').style.order = -1;
-  document.getElementById('name6').innerHTML = 'Today';
-  document.getElementById('name6').style.backgroundColor = '#FFE4B2';
-} else if(currentDay === 7) {
-
-  document.getElementById('box7').style.order = -1;
-  document.getElementById('name7').innerHTML = 'Today';
-  document.getElementById('name7').style.backgroundColor = '#FFE4B2';
-}
 }
 
-
-/*
-
-//GET DAY OF THE WEEK
-
-function myFunction() {
-    var d = new Date();
-    var n = d.getDay()
-    document.getElementById("demo").innerHTML = n;
-}
-
-//GET MONTH
-
-var d = new Date();
-var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-document.getElementById("demo").innerHTML = months[d.getMonth()];
-
-//GET DAY OF THE MONTH
-
-var d = new Date();
-document.getElementById("demo").innerHTML = d.getDate();
-
-
-*/
-
-
-/*
-    for (var i = 0; i < addArray.length; i++)
-        addArray[i].onclick = function(event)
-    {}
-
-    document.getElementById('submit-btns').addEventListener('click', function(evt) {}, false);
-
-    for (var i = 0; i < openArray.length; i++)
-        openArray[i].onclick = function(event)
-    {}
-*/
